b647c0992195128f02b2f4ac4102c23ae4fcd95c
kevin.wang <wwei@landicorp.com>
2016-05-03 10:02:48 +0800

    添加多文档的javadoc功能

---
+++ 19_javadoc/Base.java
@@ -0,0 +1,14 @@
+/**
+ * @author  kevin.wang
+ * @version V1.0.0
+ */
+public class Base {
+	private static final int mID = 10;
+
+	/**
+	 * @param value 临时设置一个数值
+	 */
+	public Base(int value) {
+		System.out.println("base create:" + mID+value);
+	}
+}
--- 19_javadoc/Main.java
+++ 19_javadoc/Main.java
@@ -1,21 +1,31 @@
 /**
  * @author  kevin.wang
- *
+ * @version V1.1.0
  * @return 返回数值为空
  */
 
 public class Main {
 	/**
-	 * @param author 输入作者是水
-	 *
-	 * @return 返回数值为NULL
+	 * @param author 输入作者的姓名
+	 * @return 目前返回数值都是true 
 	 */
-	public static void Print_Version(String author) {
+	public static boolean Print_Version(String author) {
 		System.out.println("Hello the world" + author);
+		return true;
 	}
 
+	/**
+	 *@see Base
+	 *@see Base#Base
+	 */
 	public static void main (String[] args)
 	{
-		Print_Version();
+		/** 打印的次数 */
+		int times = 3; 
+		while( times!=0 )
+		{
+			Print_Version();
+			times --;	
+		}
 	}
 }
