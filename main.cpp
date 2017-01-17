fc79f8a303d2447c2cf8aa9148d37cfb917db3f6
kevin.wang <wwei@landicorp.com>
2017-01-17 21:13:42 +0800

    进行提交

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2017年01月17日 21时13分16秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  kevin (WangWei), kevin.wang2004@hotmail.com
+ *        Company:  LiLin-Tech
+ *   Organization:  GNU
+ *
+ * =====================================================================================
+ */
+
+
