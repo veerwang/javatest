147db1da4acb9e37e9c6c3ba23b76466ccd4ac6f
kevin.wang <wwei@landicorp.com>
2019-01-18 15:03:16 +0800

    修正23的jni的类的使用

--- 22_jni_test/run.sh
+++ 22_jni_test/run.sh
@@ -22,6 +22,7 @@ set -o nounset                              # Treat unset variables as an error
 echo "Compiling project .."
 javac Main.java
 javah NativeFun				#注意是NativeFun,不是针对文件的，是针对类的。
+                                        #注意javah的生成需要在package中的路径下执
 
 g++ -I/opt/jdk1.8.0_102/include/linux -I/opt/jdk1.8.0_102/include/ -fPIC -shared -o libNativeFun.so NativeFun.cpp
 #其中include的路径通过 which java获得
