dceffa38346d47d420bd9260b10e35adae184e6c
kevin.wang <wwei@landicorp.com>
2019-01-30 09:07:50 +0800

    修正代码

--- 23_android_apk_library_jni/apk/ClientApp/app/src/main/java/com/security/landicorp/clientapp/MainActivity.java
+++ 23_android_apk_library_jni/apk/ClientApp/app/src/main/java/com/security/landicorp/clientapp/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
 		    // Following the example above for an AIDL interface,
 		    // this gets an instance of the IRemoteInterface, which we can use to call on the service
 		    mIRemoteService = IRemoteService.Stub.asInterface(service);
-		    Log.v(MainActivity.ACTIVITY_TAG,"kevin --->  Connect Remote Service");  
+	    	    Log.v(MainActivity.ACTIVITY_TAG,"kevin --->  client:ServiceConnect");  
 		    try {
 			    int id = mIRemoteService.getPid();
     			    showMessage(Integer.toString(id));
@@ -50,30 +50,31 @@ public class MainActivity extends AppCompatActivity {
 	    public void onServiceDisconnected(ComponentName className) {
 		    Log.e(MainActivity.ACTIVITY_TAG, "Service has unexpectedly disconnected");
 		    mIRemoteService = null;
+		    // 注意只有在极端的服务端crash的情况下，该函数才会被调用
+	    	    Log.v(MainActivity.ACTIVITY_TAG,"kevin --->  client:ServiceDisconnect");  
 	    }
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
 
-    	showMessage("Hello");
+    	    showMessage("Ver. 1.0.4");
 
-	Intent localIntent = new Intent();
-	//Without this, throw exception.
-	//localIntent.setPackage("com.security.landicorp.vulnerabilitydetect.RemoteService");
-	localIntent.setClassName("com.security.landicorp.vulnerabilitydetect","com.security.landicorp.vulnerabilitydetect.RemoteService");
-	//localIntent.setAction(ACTION_BIND_LOCAL_SERVICE);
-	bindService(localIntent, mRemoteConnection, BIND_AUTO_CREATE);
+	    Intent localIntent = new Intent();
+	    // 以下这句是进行远方服务的关键
+	    localIntent.setClassName("com.security.landicorp.vulnerabilitydetect","com.security.landicorp.vulnerabilitydetect.RemoteService");
+	    bindService(localIntent, mRemoteConnection, BIND_AUTO_CREATE);
 
-        Log.v(MainActivity.ACTIVITY_TAG,"kevin --->  OnCreate");  
+	    Log.v(MainActivity.ACTIVITY_TAG,"kevin --->  client:Create");  
     }
 
     @Override
     protected void onDestroy() {
-        super.onDestroy();
- 	unbindService(mRemoteConnection);
+	    super.onDestroy();
+	    unbindService(mRemoteConnection);
+	    Log.v(MainActivity.ACTIVITY_TAG,"kevin --->  client:Destroy:unbind");  
     }
 
     private void showMessage(String val)
